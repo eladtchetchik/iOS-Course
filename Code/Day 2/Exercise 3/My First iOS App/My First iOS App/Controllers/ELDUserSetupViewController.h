e652a5d0c817e77c121eb5cb4b716fc0422b25a7
Elad Tchetchik <eladtche@gmail.com>
2016-02-03 09:26:17 +0200

    Copied the app from exercise 2 and prepared for exercise 3

---
+++ Code/Day 2/Exercise 3/My First iOS App/My First iOS App/Controllers/ELDUserSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELDUSerSetupViewController.h
+//  My First iOS App
+//
+//  Created by Elad Tchetchik on 7/16/15.
+//  Copyright (c) 2015 za.co.elad. All rights reserved.
+//
+
+#import "ELDViewController.h"
+
+@interface ELDUserSetupViewController : ELDViewController
+
+@end
