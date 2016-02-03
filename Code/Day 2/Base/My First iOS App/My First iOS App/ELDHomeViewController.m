b0a7dd1df7027728e4043d2fb417d320dcc323a7
Elad Tchetchik <eladtche@gmail.com>
2016-02-03 09:17:04 +0200

    Added the base code for my first iOS app

---
+++ Code/Day 2/Base/My First iOS App/My First iOS App/ELDHomeViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  My First iOS App
+//
+//  Created by Elad Tchetchik on 7/16/15.
+//  Copyright (c) 2015 za.co.elad. All rights reserved.
+//
+
+#import "ELDHomeViewController.h"
+
+@interface ELDHomeViewController ()
+
+@end
+
+@implementation ELDHomeViewController
+
+- (void)viewDidLoad
+    {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+- (void)didReceiveMemoryWarning
+    {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    }
+
+@end
